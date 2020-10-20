cede606c7be48c3c86f67b89596455360751a89c
AFS12 <arthurmbayres@gmail.com>
2020-10-20 18:57:19 -0300

    grupo de rotas aula ate o minuto 12:50

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/login', function () {
+    return "Login";
+})-> name('login');
+
+Route::middleware([])->group(function(){
+
+    Route::prefix('admin')-> group(function(){
+
+        Route::namespace('App\Http\Controllers\Admin\TesteController@teste')->group(function(){
+        
+        Route::get('/dashboard', 'App\Http\Controllers\Admin\TesteController@teste');
+        
+        Route::get('/financeiro', 'App\Http\Controllers\Admin\TesteController@teste');
+        
+        Route::get('/produtos', 'App\Http\Controllers\Admin\TesteController@teste');
+
+        Route::get('/', 'App\Http\Controllers\Admin\TesteController@teste');
+        });
+    });
+});
+
+
+
 Route::get('redirect3', function () {
     return redirect()->route('url.name');
 });
