d5de03651fe308589338b824cbda8d5cea9ee319
AFS12 <arthurmbayres@gmail.com>
2020-10-21 11:18:18 -0300

    Estruturas de controle blade parte 2

--- resources/views/admin/pages/products/index.blade.php
+++ resources/views/admin/pages/products/index.blade.php
@@ -4,29 +4,58 @@
 
 @section('content')
 
-<h1>EXIBINDO PRODUTOS</h1>
-@if ($teste === '123')
-    É igual
-@elseif ($teste == 123)
-    É igual a 123
-@else
-    É diferente
-@endif
-    
-@unless ($teste === 123)
-    subarashii
-@else
-    djfjdsafao
-@endunless
+    <h1>EXIBINDO PRODUTOS</h1>
+    @if ($teste === '123')
+        É igual
+    @elseif ($teste == 123)
+        É igual a 123
+    @else
+        É diferente
+    @endif
+        
+    @unless ($teste === 123)
+        true
+    @else
+        djfjdsafao
+    @endunless
 
-@isset($teste2)
-    <p>{{$teste2}}</p>
-    
-@endisset
+    @isset($teste2)
+        <p>{{$teste2}}</p>
+        
+    @endisset
 
-@empty($teste3)
-    osadoijfdsaofodsaofds
-@endempty
+    @empty($teste3)
+        osadoijfdsaofodsaofds
+    @endempty
+
+    @auth
+        <p>Autenticado</p>
+    @else
+        <p>Não autenticado</p>
+    @endauth
+
+    @guest
+        <p>Bem vindo visitante</p>
+    @else
+        Bem vindo de volta
+    @endguest
+
+    @switch($teste)
+        @case(1)
+            igual 1
+            @break
+        @case(2)
+            igual 2
+            @break
+        @case(3)
+            igual 3
+            @break
+        @case(123)
+            igual 123
+            @break
+        @default
+            Default
+    @endswitch
 
 @endsection
 
