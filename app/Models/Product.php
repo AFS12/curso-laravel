960a5f2f711716900f0caf78c50bab75832f4d4e
AFS12 <arthurmbayres@gmail.com>
2020-10-22 18:28:03 -0300

    Factory de produtos

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'image',
+    ];
 }
